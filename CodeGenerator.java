d0b834e8347ded499014773e902f08ae10904d3c
Gilbert Chan <unusgilbert@gmail.com>
2017-05-04 02:08:12 -0700

    Add code for constants, registers

--- CodeGenerator.java
+++ CodeGenerator.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
-class CodeGenerator implements AATVisitor { 
-    
+class CodeGenerator implements AATVisitor {
+
     public CodeGenerator(String output_filename) {
 	try {
 	    output = new PrintWriter(new FileOutputStream(output_filename));
@@ -11,72 +11,103 @@ class CodeGenerator implements AATVisitor {
 	/*  Feel free to add code here, if you want to */
 	EmitSetupCode();
     }
-  
-    public Object VisitCallExpression(AATCallExpression expression) { 
-    }
-  
-    public Object VisitMemory(AATMemory expression) { 
-    }
-    
-    
-    public Object VisitOperator(AATOperator expression) { 
+
+    public Object VisitCallExpression(AATCallExpression expression) {
+		return null;
     }
 
-    public Object VisitRegister(AATRegister expression) { 
+    public Object VisitMemory(AATMemory expression) {
+		return null;
     }
-    
+
+
+    public Object VisitOperator(AATOperator expression) {
+        expression.left().Accept(this);
+        expression.right().Accept(this);
+
+        if(expression.operator() == AATOperator.PLUS) {
+            emit("lw $t1, 8($ESP)");
+            emit("lw $t2, 4($ESP)");
+            emit("add $t1, $t1, $t2");
+            emit("sw $t1, 8($ESP)");
+            emit("add $ESP, $ESP, 4");
+        } else if (expression.operator() == AATOperator.MINUS) {
+            emit("lw $t1, 8($ESP)");
+            emit("lw $t2, 4($ESP)");
+            emit("sub $t1, $t1, $t2");
+            emit("sw $t1, 8($ESP)");
+            emit("add $ESP, $ESP, 4");
+        }
+
+		return null;
+    }
+
+    public Object VisitRegister(AATRegister expression) {
+        emit("sw " + expression.register().toString() + ", 0($ESP)");
+        emit("addi $ESP, $ESP, -4");
+		return null;
+    }
+
     public Object VisitCallStatement(AATCallStatement statement) {
+        return null;
     }
     public Object VisitConditionalJump(AATConditionalJump statement) {
+        return null;
     }
-    
+
     public Object VisitEmpty(AATEmpty statement) {
+        return null;
     }
     public Object VisitJump(AATJump statement) {
-	emit("j " + statement.label());
-	return null;
+        emit("j " + statement.label());
+        return null;
     }
     public Object VisitLabel(AATLabel statement) {
-	emit(statement.label() + ":");
-	return null;
+        emit(statement.label() + ":");
+        return null;
     }
     public Object VisitMove(AATMove statement) {
-	if (statement.lhs() instanceof AATMemory)
-	    {
-		((AATMemory)statement.lhs()).mem().Accept(this);
-//                 <emit code to store acc on expression stack>
-		      statement.rhs().Accept(this);
-//                 <code to pop value off expression stack to T1>
-		      emit("sw " + Register.ACC() + " 0(" + Register.Tmp1() + ")");
-		
-	    }
+        if (statement.lhs() instanceof AATMemory) {
+            ((AATMemory)statement.lhs()).mem().Accept(this);
+            // <emit code to store acc on expression stack>
+            statement.rhs().Accept(this);
+            // <code to pop value off expression stack to T1>
+            emit("sw " + Register.ACC() + " 0(" + Register.Tmp1() + ")");
 
+        }
 
+        return null;
     }
     public Object VisitReturn(AATReturn statement) {
-	emit("jr " + Register.ReturnAddr());
-	return null;
+        emit("jr " + Register.ReturnAddr());
+        return null;
     }
 
     public Object VisitHalt(AATHalt halt) {
-	/* Don't need to implement halt -- you can leave 
+	/* Don't need to implement halt -- you can leave
 	   this as it is, if you like */
-	return null;
+	    return null;
     }
     public Object VisitSequential(AATSequential statement) {
+        return null;
     }
-    
+
     public Object VisitConstant(AATConstant expression) {
+        emit("addi $t1, $zero, " + expression.value());
+        emit("sw $t1, 0($ESP)");
+        emit("addi $ESP, $ESP, -4");
+
+        return null;
     }
-    
+
     private void emit(String assem) {
-	assem = assem.trim();
-	if (assem.charAt(assem.length()-1) == ':') 
-      output.println(assem);
-	else
-	    output.println("\t" + assem);
+        assem = assem.trim();
+        if (assem.charAt(assem.length()-1) == ':')
+          output.println(assem);
+        else
+            output.println("\t" + assem);
     }
-    
+
     public void GenerateLibrary() {
 	emit("Print:");
 	emit("lw $a0, 4(" + Register.SP() + ")");
@@ -111,26 +142,25 @@ class CodeGenerator implements AATVisitor {
 	emit(".word 0");
 	output.flush();
     }
-    
+
     private void EmitSetupCode() {
 	emit(".globl main");
 	emit("main:");
 	emit("addi " + Register.ESP() + "," + Register.SP() + ",0");
-	emit("addi " + Register.SP() + "," + Register.SP() + "," + 
+	emit("addi " + Register.SP() + "," + Register.SP() + "," +
 	     - MachineDependent.WORDSIZE * STACKSIZE);
 	emit("addi " + Register.Tmp1() + "," + Register.SP() + ",0");
-	emit("addi " + Register.Tmp1() + "," + Register.Tmp1() + "," + 
+	emit("addi " + Register.Tmp1() + "," + Register.Tmp1() + "," +
 	     - MachineDependent.WORDSIZE * STACKSIZE);
 	emit("la " + Register.Tmp2() + ", HEAPPTR");
 	emit("sw " + Register.Tmp1() + ",0(" + Register.Tmp2() + ")");
-        emit("sw " + Register.ReturnAddr() + "," + MachineDependent.WORDSIZE  + "("+ Register.SP() + ")"); 
+        emit("sw " + Register.ReturnAddr() + "," + MachineDependent.WORDSIZE  + "("+ Register.SP() + ")");
  	emit("jal main1");
 	emit("li $v0, 10");
         emit("syscall");
     }
-    
+
     private final int STACKSIZE = 1000;
     private PrintWriter output;
     /* Feel Free to add more instance variables, if you like */
 }
-
